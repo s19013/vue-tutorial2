f93f9a4f541add0e5ffec9141cb2c8dd59521c97
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-05 00:08:46 +0900

    一旦保存

--- grand-child-component.js
+++ grand-child-component.js
@@ -1,5 +1,13 @@
 export const grandChildComponent = {
     template:`
-    <div>孫コンポーネント</div>
-    `
+    <div>
+    孫コンポーネント
+    <button @click="testEvent">ボタン</button>
+    </div>
+    `,
+    methods: {
+        testEvent(){
+            eventBus.$emit('eventBus-test','孫のボタンをおしました')
+        }
+    },
 }
